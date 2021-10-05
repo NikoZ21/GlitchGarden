7d6e5a83277a6d60b5af65a1ba74deb90ac17942
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2021-10-06 01:55:22 +0400

    added defender spawning system

--- Assets/Scripts/SpawnDefenders.cs
+++ Assets/Scripts/SpawnDefenders.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,27 @@ public class SpawnDefenders : MonoBehaviour
 
     private void OnMouseDown()
     {
-        GameObject newDefender = Instantiate(cactus, transform.position, transform.rotation) as GameObject;
+        SpawnDefender(GetSquareClicked());
+
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = snapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 snapToGrid(Vector2 worldPos)
+    {
+        float newX = Mathf.RoundToInt(worldPos.x);
+        float newY = Mathf.RoundToInt(worldPos.y) + 0.2f;
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 worlPos)
+    {
+        GameObject newDefender = Instantiate(cactus, worlPos, transform.rotation) as GameObject;
     }
 }
